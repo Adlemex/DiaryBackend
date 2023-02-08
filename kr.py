7d13c6045ca437d7ea503aa9b6a28b5dd14ee177
Adlemex <87426336+Adlemex@users.noreply.github.com>
2023-02-08 14:54:18 +0300

    Update kr.py

--- kr.py
+++ kr.py
@@ -1,5 +1,5 @@
 from firebase_admin import credentials, messaging
-
+import firebase_admin
 cred = credentials.Certificate("./diary-b2ba2-firebase-adminsdk-nvon7-1be3dce4d4.json")
 default_app = firebase_admin.initialize_app(cred)
 topic = "kr_" + str(18)
